ce91b5f96f6c589723c0eb426d1594a4c29db091
whitespine <jakehenry.tn@gmail.com>
2022-01-26 03:13:28 -0500

    Initial code

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,26 @@
 Hooks.once('init', async function() {
+	for(let bar=0; bar<=4; bar++) {
+		for(let slot=0; slot<=9; slot++) {
+			let macro_slot = bar ? `${bar}${slot}` : `${slot}`;
 
+			game.keybindings.register("core", `absHotbarBind${macro_slot}`, {
+				name: `Bar ${bar + 1} macro slot ${slot}`,
+				hint: `Pressing this will execute the macro in hotbar slot ${slot} of bar ${bar + 1}`,
+				onDown: () => {
+					let macro_id = game.user.data.hotbar[macro_slot];
+					if(macro_id) {
+						let macro = game.macros.get(macro_id);
+						if(macro) {
+							// Execute it
+							macro.execute();
+						}
+					}
+				},
+				onUp: () => {},
+				precedence: CONST.KEYBINDING_PRECEDENCE.NORMAL
+			});
+		}
+	}
 });
 
 Hooks.once('ready', async function() {
